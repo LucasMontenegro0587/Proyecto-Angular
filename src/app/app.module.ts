bac9b53994f7282e90e6cfa9e6c715acb097792c
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-10-08 02:03:48 -0300

    Creación de app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListaAlumnosComponent } from './alumnos/lista-alumnos/lista-alumnos.component';
+import { AbmAlumnosComponent } from './alumnos/abm-alumnos/abm-alumnos.component';
+import { ListaCursosComponent } from './cursos/lista-cursos/lista-cursos.component';
+import { AbmCursosComponent } from './cursos/abm-cursos/abm-cursos.component';
+import { ListaInscripcionesComponent } from './inscripciones/lista-inscripciones/lista-inscripciones.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaAlumnosComponent,
+    AbmAlumnosComponent,
+    ListaCursosComponent,
+    AbmCursosComponent,
+    ListaInscripcionesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule // Asegurarse de que el módulo esté importado
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
