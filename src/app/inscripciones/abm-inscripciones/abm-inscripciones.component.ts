5c632aab7795a4ad7565a0cb05c220ed5f824081
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-10-08 02:36:48 -0300

    Modificación de archivo - Parte III

--- src/app/inscripciones/abm-inscripciones/abm-inscripciones.component.ts
+++ src/app/inscripciones/abm-inscripciones/abm-inscripciones.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-abm-inscripciones',
-  standalone: true,
-  imports: [],
   templateUrl: './abm-inscripciones.component.html',
-  styleUrl: './abm-inscripciones.component.scss'
+  styleUrls: ['./abm-inscripciones.component.scss']
 })
-export class AbmInscripcionesComponent {
+export class AbmInscripcionesComponent implements OnInit {
 
+  inscripcionForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.inscripcionForm = this.fb.group({
+      alumno: ['', Validators.required],
+      curso: ['', Validators.required],
+      fecha: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void { }
+
+  onSubmit() {
+    if (this.inscripcionForm.valid) {
+      console.log(this.inscripcionForm.value);
+      // Debería agregar la lógica para guardar los datos de la inscripción
+    }
+  }
 }
--- src/app/inscripciones/lista-inscripciones/lista-inscripciones.component.ts
+++ src/app/inscripciones/lista-inscripciones/lista-inscripciones.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Inscripcion {
+  id: number;
+  alumno: string;
+  curso: string;
+  fecha: string;
+}
 
 @Component({
   selector: 'app-lista-inscripciones',
-  standalone: true,
-  imports: [],
   templateUrl: './lista-inscripciones.component.html',
-  styleUrl: './lista-inscripciones.component.scss'
+  styleUrls: ['./lista-inscripciones.component.scss']
 })
-export class ListaInscripcionesComponent {
+export class ListaInscripcionesComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'alumno', 'curso', 'fecha'];
+  inscripciones: Inscripcion[] = [
+    { id: 1, alumno: 'Lucas Leonel Montenegro Burgos', curso: 'Angular', fecha: '2024-10-06' },
+    { id: 2, alumno: 'Martín Emiliano Bruno', curso: 'Desarrollo Web', fecha: '2024-10-07' },
+    { id: 3, alumno: 'Demetria Devonne Lovato', curso: 'Desarrollo Web', fecha: '2024-10-07' },
+    { id: 4, alumno: 'Victoria Pedretti', curso: 'Desarrollo Web', fecha: '2024-10-07' },
+    { id: 5, alumno: 'Chloe Lewis', curso: 'CoffeeScript', fecha: '2024-11-01' },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
