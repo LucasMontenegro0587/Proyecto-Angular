0822f7d5c8facfe04e5bca0aca990d90393e432e
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-10-09 03:46:23 -0300

    Creación de Directiva

---
+++ src/app/directives/tamanio-letra.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TamanioLetraDirective } from './tamanio-letra.directive';
+
+describe('TamanioLetraDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TamanioLetraDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/tamanio-letra.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appTamanioLetra]',
+  standalone: true
+})
+export class TamanioLetraDirective {
+
+  constructor() { }
+
+}
