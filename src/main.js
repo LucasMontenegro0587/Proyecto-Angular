e9a2d3f052996f0521b7c0376f96dc1958997b1a
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-10-11 19:55:32 -0300

    Interacciones

---
+++ src/main.js
@@ -0,0 +1,11 @@
+function iniciarSesion() {
+  alert('Se debe implementar mejora!');
+}
+
+function cerrarSesion() {
+  alert('Se debe implementar mejora!');
+}
+
+function inscribirse(curso) {
+  alert(`Te has inscrito en el curso: ${curso}`);
+}
