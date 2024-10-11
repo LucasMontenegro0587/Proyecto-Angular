68355d855f52f695895a43104eb4c17f123db003
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-10-10 21:22:24 -0300

    Revisión del Proyecto - Parte IV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'; // Módulo de barra de herramientas
+import { MatListModule } from '@angular/material/list'; // Módulo de lista de navegación
+import { NgIf } from '@angular/common'; // Asegurando NgIf en caso de condicionales futuros
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatListModule, NgIf], // Importaciones aseguradas
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Pentahouse';
