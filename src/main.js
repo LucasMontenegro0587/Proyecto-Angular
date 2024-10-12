54efb043cb2c356d7d5e13aa62ef1ce6b326456d
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-10-11 22:26:56 -0300

    Revisión del Script - Parte III

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ function iniciarSesion() {
 
 // Función para cerrar sesión
 function cerrarSesion() {
-  // Confirmación para cerrar sesión
+  // Confirmación del cierre de sesión
   const deseaCerrar = confirm("¿Querés cerrar sesión?");
   
   if (deseaCerrar) {
@@ -21,14 +21,30 @@ function cerrarSesion() {
   }
 }
 
-// Función para inscribirse a un curso
+// Función para inscribirse a un curso usando SweetAlert
 function inscribirse(curso) {
-  // Preguntamos si desea inscribirse al curso
-  const deseaInscribirse = confirm(`¿Deseás inscribirte en el curso: ${curso}?`);
-
-  if (deseaInscribirse) {
-    alert(`Inscripción exitosa al curso: ${curso}`);
-  } else {
-    alert(`Se canceló la inscripción al curso: ${curso}`);
-  }
+  // Se utiliza el SweetAlert para preguntar al usuario
+  Swal.fire({
+    title: `¿Deseás inscribirte en el curso: ${curso}?`,
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Sí, deseo inscribirme',
+    cancelButtonText: 'No, quiero cancelar la inscripción',
+  }).then((result) => {
+    // Si el usuario confirma, se muestra el mensaje de inscripción exitosa
+    if (result.isConfirmed) {
+      Swal.fire(
+        '¡La inscripción fué exitosa!',
+        `Estás inscripto en el curso: ${curso}`,
+        'Vacante ocupada'
+      );
+    } else {
+      // Si el usuario cancela, se muestra el mensaje de cancelación
+      Swal.fire(
+        'Inscripción cancelada',
+        `No estás inscripto en el curso: ${curso}`,
+        'Vacante liberada'
+      );
+    }
+  });
 }
